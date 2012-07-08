0e1349d6ee266f079153a32ece6206523ac43158
Denis Costa <deniscostadsc@gmail.com>
2012-07-07 22:18:26 -0300

    Putting blank lines between instalations.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -8,13 +8,13 @@ function exiting(){
 
 [ "$USER" != root ] && exiting "You are not root!"
 
-apt-get install -y ack-grep
-apt-get install -y git
-apt-get install -y python-pip
-apt-get install -y ruby
-apt-get install -y terminator
-apt-get install -y vim-gnome
-apt-get install -y virtualbox
+apt-get install -y ack-grep; echo -e '\n\n'
+apt-get install -y git; echo -e '\n\n'
+apt-get install -y python-pip; echo -e '\n\n'
+apt-get install -y ruby; echo -e '\n\n'
+apt-get install -y terminator; echo -e '\n\n'
+apt-get install -y vim-gnome; echo -e '\n\n'
+apt-get install -y virtualbox; echo -e '\n\n'
 
 if which pip > /dev/null; then
     pip install virtualenvwrapper
