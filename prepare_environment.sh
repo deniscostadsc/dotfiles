dc84c16c76a7dbb73b70111c326874864793d06f
Denis Costa <deniscostadsc@gmail.com>
2012-07-09 18:53:31 -0300

    Sorting installation lines.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -7,8 +7,8 @@ apt-get update
 apt-get install -y ack-grep
 apt-get install -y git
 apt-get install -y gitk
-apt-get install -y python-pip
 apt-get install -y python-dev
+apt-get install -y python-pip
 apt-get install -y ruby
 apt-get install -y rubygems
 apt-get install -y terminator
