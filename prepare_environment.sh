1d568b621330b168c7aee96b409babeb3d162584
Denis Costa <deniscostadsc@gmail.com>
2012-07-10 15:36:35 -0300

    Adding pep8 to environment.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -8,6 +8,7 @@ apt-get install -y ack-grep
 apt-get install -y git
 apt-get install -y gitg
 apt-get install -y nodejs
+apt-get install -y pep8
 apt-get install -y python-dev
 apt-get install -y python-pip
 apt-get install -y ruby
