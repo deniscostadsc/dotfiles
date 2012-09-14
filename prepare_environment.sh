f236382f10e23a2012f3b131b44c78de555213be
Denis Costa <deniscostadsc@gmail.com>
2012-09-14 13:59:22 -0300

    Adding lua to my environment.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -31,6 +31,7 @@ apt-get install -y gimp
 apt-get install -y git
 apt-get install -y gitg
 apt-get install -y inkscape
+apt-get install -y lua5.1
 apt-get install -y nodejs
 apt-get install -y pyflakes
 apt-get install -y python-dev
