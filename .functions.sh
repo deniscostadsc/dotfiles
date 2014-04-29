f330440cec5746025f54498c765df3d936258921
Denis Costa <deniscostadsc@gmail.com>
2014-04-29 14:44:01 -0300

    Adding a command to record screencasts. Thanx @tonylampada.

--- .functions.sh
+++ .functions.sh
@@ -6,6 +6,11 @@ function rmpyc {
     find . -name "*.pyc" -delete;
 }
 
+function screencast {
+    now=$(date +"%Y%m%d-%H%M%S")
+    ffmpeg -f x11grab -s 1366x768 -r 25 -i :0.0 -acodec pcm_s16le -sameq -f alsa -ac 2 -i pulse ~/screencast_$now.mkv
+}
+
 function stripy {
     # Remove spaces from end of lines
     if [ $# -gt 0 ]; then
