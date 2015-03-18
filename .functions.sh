3cba6e970a98074f526a0c52b862fa2778127e5b
Denis Costa <deniscostadsc@gmail.com>
2015-03-18 16:26:00 -0300

    Extract screencast to a separate command.

--- .functions.sh
+++ .functions.sh
@@ -6,11 +6,6 @@ function rmpyc {
     find . -name "*.pyc" -delete;
 }
 
-function screencast {
-    now=$(date +"%Y%m%d-%H%M%S")
-    ffmpeg -f x11grab -s 1366x768 -r 25 -i :0.0 -acodec pcm_s16le -sameq -f alsa -ac 2 -i pulse ~/screencast_$now.mkv
-}
-
 function __git_rm {
     git rm $* 2> /dev/null || trash $*
 }
