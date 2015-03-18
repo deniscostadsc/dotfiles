edc8050b39b864218fdae8200fdcc84ce821facf
Denis Costa <deniscostadsc@gmail.com>
2015-03-18 14:04:51 -0300

    Add meld o environment.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -13,6 +13,7 @@ apt-get install -y git
 apt-get install -y gitg
 apt-get install -y gummi
 apt-get install -y inkscape
+apt-get install -y meld
 apt-get install -y openjdk-7-jdk
 apt-get install -y python-dev
 apt-get install -y python-setuptools
