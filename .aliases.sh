ba428f1427e2748fce77f3b7017f807a7f744810
Denis Costa <deniscostadsc@gmail.com>
2016-02-24 12:36:34 -0300

    Fixes aliases when in OSXhit.

--- .aliases.sh
+++ .aliases.sh
@@ -1,10 +1,4 @@
-alias ack='ack-grep'
-
 alias reload_bashrc='. ~/.bashrc'
-
-if is_linux; then
-    alias ls='ls --color=auto'
-fi
 
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
@@ -15,3 +9,8 @@ alias ll='ls -alhF'
 alias rm='__git_rm'
 alias mv='__git_mv'
 alias git-remove-branchs='git branch -D $(git branch | grep -v "*\|master")'
+
+if is_linux; then
+    alias ack='ack-grep'
+    alias ls='ls --color=auto'
+fi
