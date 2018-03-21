f129a302ddfb4d54f1b2882290197add4afcd9b9
Denis Costa <deniscostadsc@gmail.com>
2018-03-21 17:49:20 -0300

    Improve gitandpull function

--- recipes/git.sh
+++ recipes/git.sh
@@ -1,9 +1,12 @@
 kickstart.git.cloneandpull() {
-    [[ ! -d $2 ]] && \
-        git clone $1 $2
+    [[ ! -d $2 ]] && {
+        kickstart.info "Cloning $1"
+        kickstart.mute git clone $1 $2
+    }
 
-    cd $2
-    git pull origin master
-    git reset --hard origin/master
-    cd -
+    kickstart.mute cd $2
+    kickstart.info "Fetchcing $1"
+    kickstart.mute git fetch --all
+    kickstart.mute git reset --hard origin/master
+    kickstart.mute cd -
 }
