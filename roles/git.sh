4854cda0008678bb8ba371fea9dfac28623131ee
Denis Costa <deniscostadsc@gmail.com>
2018-03-21 15:30:46 -0300

    Finish git role

--- roles/git.sh
+++ roles/git.sh
@@ -6,14 +6,41 @@ kickstart.package.install git
 kickstart.package.install gitg
 kickstart.package.install meld
 
-cp files/git/globalignore ~/.config/git/ignore
-cp files/git/gitconfig ~/.gitconfig
-cp files/git/functions.sh ~/.bashrc_git.sh
-echo '. ~/.bashrc_git.sh' >> ~/.bashrc
+kickstart.info "Copy git config files"
+cp --preserve=mode,ownership files/git/globalignore ~/.config/git/ignore
+cp --preserve=mode,ownership files/git/gitconfig ~/.gitconfig
 
+kickstart.info "Copy hook to git template folder"
 kickstart.mute mkdir -p ~/.git_template/hooks || true
-cp files/git/hooks/* ~/.git_template/hooks/
+cp --preserve=mode,ownership files/git/hooks/* ~/.git_template/hooks/
 
-#TODO
+echo  >> ~/.bashrc
+echo "# Git - added by kickstart" >> ~/.bashrc
 
-alias git-remove-branchs='git branch -D $(git branch | grep -v "*\|master")'
+kickstart.info "Add aliases to bashrc"
+echo "alias git-remove-branchs='git branch -D $(git branch | grep -v "*\|master")'" >> ~/.bashrc
+
+kickstart.info "Add functions to bashrc"
+echo 'function __git_arrows {
+    git rev-parse --abbrev-ref @"{u}" &>/dev/null || return
+    local branch_status
+
+    branch_status="$(git rev-list --left-right --count HEAD...@"{u}" 2>/dev/null)"
+
+    [ $? == 0 ] || return
+    local left_arrow
+    local right_arrow
+
+    left_arrow=$(echo $branch_status | cut -f 1 -d " ")
+    right_arrow=$(echo $branch_status | cut -f 2 -d " ")
+
+    local arrows
+    [ $left_arrow != 0 ] && arrows="⇡"
+    [ $right_arrow != 0 ] && arrows="${arrows}⇣"
+
+    echo $arrows
+}' >> ~/.bashrc
+
+echo 'function __git_branch {
+    echo -n "$(git branch 2>/dev/null | grep '^*' | sed 's/* //g')"
+}' >> ~/.bashrc
