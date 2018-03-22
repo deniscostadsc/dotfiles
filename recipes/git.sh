ef1a7a04e5463cf2da781cb094de4af5046efb70
Denis Costa <deniscostadsc@gmail.com>
2018-03-22 14:34:52 -0300

    Fix a few things on recipes/git.sh

--- recipes/git.sh
+++ recipes/git.sh
@@ -1,12 +1,12 @@
 kickstart.git.cloneandpull() {
     [[ ! -d $2 ]] && {
         kickstart.info "Cloning $1"
-        kickstart.mute git clone $1 $2
+        kickstart.mute git clone "$1 $2"
     }
 
-    kickstart.mute cd $2
-    kickstart.info "Fetchcing $1"
+    kickstart.mute cd "$2" || exit 1
+    kickstart.info "Fetching $1"
     kickstart.mute git fetch --all
     kickstart.mute git reset --hard origin/master
-    kickstart.mute cd -
+    kickstart.mute cd - || exit 1
 }
