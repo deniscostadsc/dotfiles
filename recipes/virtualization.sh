1bd96dd9a994abb9460cb559eef5749a02cb6ab9
Denis Costa <deniscostadsc@gmail.com>
2018-03-22 14:32:33 -0300

    Finish virtualization

---
+++ recipes/virtualization.sh
@@ -0,0 +1,22 @@
+__get_vagrant_latest_version() {
+    curl -s https://releases.hashicorp.com/vagrant/ |\
+        sed 's/<[^>]*>\|[[:space:]]//g' |\
+        grep vagrant |\
+        head -1
+}
+
+kickstart.package.install_vagrant() {
+    package=$(__get_vagrant_latest_version)
+    version=$(sed 's/[a-z_]*//g' <<< "$package")
+
+    wget -s "https://releases.hashicorp.com/vagrant/$version/${package}_x86_64.deb"
+    wget -s "https://releases.hashicorp.com/vagrant/$version/${package}_SHA256SUMS"
+    wget -s "https://releases.hashicorp.com/vagrant/$version/${package}_SHA256SUMS.sig"
+
+    gpg --verify "${package}_SHA256SUMS.sig" "${package}_SHA256SUMS"
+    sha256sum -c <(grep "64.deb" "${package}_SHA256SUMS")
+
+    apt install "./${package}_x86_64.deb"
+
+    rm -rf vagrant*
+}
--- roles/virtualization.sh
+++ roles/virtualization.sh
@@ -2,4 +2,17 @@
 
 kickstart.context 'Virtualization'
 
+source recipes/virtualization.sh
+
+kickstart.command_exists gpg || {
+    echo "This role depends on GnuPG role"
+    exit 1
+}
+
+kickstart.apt.add_key_from_url https://www.virtualbox.org/download/oracle_vbox_2016.asc
+kickstart.apt.add_key_from_url https://www.virtualbox.org/download/oracle_vbox.asc
+
+kickstart.package.update
+
 kickstart.package.install virtualbox
+kickstart.package.install_vagrant
