aca1253bd0cca654143572a15261ef27b9e96dc1
Denis Costa <deniscostadsc@gmail.com>
2018-06-07 14:35:14 -0300

    Check if OS is Ubuntu before remove apt cache

--- roles/os.sh
+++ roles/os.sh
@@ -2,9 +2,12 @@
 
 kickstart.context 'OS'
 
+kickstart.info "Update OS"
 kickstart.package.update
 kickstart.package.upgrade
 
-kickstart.info "Remove cached and unused packages"
-kickstart.mute apt -y autoclean
-kickstart.mute apt -y autoremove
+kickstart.os.is "Ubuntu" && {
+    kickstart.info "Remove cached and unused packages"
+    kickstart.mute apt -y autoclean
+    kickstart.mute apt -y autoremove
+}
