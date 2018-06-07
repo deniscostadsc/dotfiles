e5ca7811498a2a59ac3b84f9b2dd9c60bea2281a
Denis Costa <deniscostadsc@gmail.com>
2018-06-07 14:29:01 -0300

    Add jq

--- roles/dev-tools.sh
+++ roles/dev-tools.sh
@@ -3,6 +3,7 @@
 kickstart.context 'Dev Tools'
 
 kickstart.package.install curl
+kickstart.package.install jq
 kickstart.package.install shellcheck
 kickstart.package.install tree
 kickstart.package.install wget
