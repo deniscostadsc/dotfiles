b1f9d78f99bcf1b50595bba53cdeb70714e25a98
Denis Costa <deniscostadsc@gmail.com>
2018-06-07 14:21:52 -0300

    Rename scripts to have easier lint

--- roles/bash.sh
+++ roles/bash.sh
@@ -12,7 +12,12 @@ kickstart.info "Create ~/.bin folder if does not exist"
 kickstart.mute mkdir ~/.bin || true
 
 kickstart.info "Copy bin files to bin folder"
-cp --preserve=mode,ownership files/bash/bin/* ~/.bin
+cp --preserve=mode,ownership files/bash/bin/beep.sh ~/.bin/beep
+cp --preserve=mode,ownership files/bash/bin/colorize-code.sh ~/.bin/colorize-code
+cp --preserve=mode,ownership files/bash/bin/colors.sh ~/.bin/colors
+cp --preserve=mode,ownership files/bash/bin/diceware.sh ~/.bin/diceware
+cp --preserve=mode,ownership files/bash/bin/screencast.sh ~/.bin/screencast
+cp --preserve=mode,ownership files/bash/bin/trim.sh ~/.bin/trim
 
 kickstart.file.append_once ~/.bashrc ""
 kickstart.file.append_once ~/.bashrc "# Bash - added by kickstart"
