e4dfabdc68517cf100701364f6331689ce05ff04
Denis Costa <deniscostadsc@gmail.com>
2018-06-14 10:49:17 -0300

    Remove a lot of things on Irssi notification

--- files/communication/irssi/notify-listener.py
+++ files/communication/irssi/notify-listener.py
@@ -16,23 +16,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import os, sys
 try:
     import gi
     gi.require_version('Notify', '0.7')
     from gi.repository import Notify, GObject
-except:
+except Exception:
+    import sys
     print >> sys.stderr, 'Could not locate pygobject'
     sys.exit(-1)
 import dbus
 import dbus.mainloop.glib
-from subprocess import call
 
-player = None
-for path in os.environ['PATH'].split(os.pathsep):
-    if os.path.isfile(os.path.join(path, 'canberra-gtk-play')):
-        player = os.path.join(path, 'canberra-gtk-play')
-        break
 
 class IrssiListener:
     def __init__(self):
@@ -49,21 +43,14 @@ class IrssiListener:
         n = Notify.Notification.new(subject.encode('utf-8'),
                                     message.encode('utf-8'),
                                     'dialog-information')
-        # Concatenate messages for certain distros
         n.set_hint_string('x-canonical-append', 'true')
         n.show()
-
-        # It would be more kosher to do this with GStreamer, but in
-        # the interest of time:
-        if player is not None:
-            retcode = call([player, '-i', 'message-new-instant'])
-            if retcode < 0:
-                print "ERROR: Player returned code %d" % (retcode)
 
     def __del__(self):
         Notify.uninit()
 
+
 if __name__ == '__main__':
-    l = IrssiListener()
+    listener = IrssiListener()
     loop = GObject.MainLoop()
     loop.run()
