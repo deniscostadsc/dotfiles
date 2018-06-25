a79a072abb4d80124d527fab85bfc5e48af54359
Denis Costa <deniscostadsc@gmail.com>
2018-06-25 19:41:39 -0300

    Move kickstart config from role to rc-file

--- roles/kickstart.sh
+++ roles/kickstart.sh
@@ -13,12 +13,7 @@ kickstart.package.install git
 
 kickstart.git.cloneandpull https://github.com/bltavares/kickstart.git ~/.kickstart
 
-kickstart.file.append_once ~/.bashrc ""
-kickstart.file.append_once ~/.bashrc "# Kickstart - added by kickstart"
-
-kickstart.info "Add code complete to bash"
-# shellcheck disable=SC2016
-kickstart.file.append_once ~/.bashrc 'eval "$(kickstart autocomplete)"'
+kickstart.file.append_file_once files/kickstart/bashrc_kickstart.sh ~/.bashrc
 
 kickstart.info "Create kickstart bin link"
 kickstart.file.link ~/.kickstart/bin/kickstart ~/.bin/kickstart
