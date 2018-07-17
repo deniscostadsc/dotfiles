0faad45b72c8f7d1bd218ea3437db0e18424673a
Denis Costa <deniscostadsc@gmail.com>
2018-07-17 09:31:50 -0300

    Fix git clone function

--- recipes/git.sh
+++ recipes/git.sh
@@ -3,7 +3,7 @@
 kickstart.git.cloneandpull() {
     [[ ! -d $2 ]] && {
         kickstart.info "Cloning $1"
-        kickstart.mute git clone "$1 $2"
+        kickstart.mute git clone "$1" "$2"
     }
 
     kickstart.mute cd "$2" || exit 1
