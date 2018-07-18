756f24db5cd02aa014779985203766f46716cbcc
Denis Costa <deniscostadsc@gmail.com>
2018-07-18 18:06:25 -0300

    Add htop

---
+++ files/dev-tools/bashrc_dev-tools.sh
@@ -0,0 +1,1 @@
+alias top='echo -e "\nUse htp instead of top\n\n"'
--- roles/dev-tools.sh
+++ roles/dev-tools.sh
@@ -5,8 +5,11 @@ kickstart.context 'Dev Tools'
 kickstart.package.update
 
 kickstart.package.install curl
+kickstart.package.install docker.io
+kickstart.package.install htop
 kickstart.package.install jq
 kickstart.package.install shellcheck
 kickstart.package.install tree
 kickstart.package.install wget
-kickstart.package.install docker.io
+
+kickstart.file.append_file_once files/tmux/bashrc_dev-tools.sh $HOME/.bashrc
