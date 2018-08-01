e74c40c38973ddf0699e58746b68abc8732a1df4
Denis Costa <deniscostadsc@gmail.com>
2018-08-01 10:27:49 -0300

    Run brew before to avoid OSX conflicts

--- recipes/os.sh
+++ recipes/os.sh
@@ -1,15 +1,15 @@
 generic_install() {
     # shellcheck disable=SC2048
     # shellcheck disable=SC2086
-    apt install -y "$*" > /dev/null 2>&1 || \
     brew install -y "$*" > /dev/null 2>&1 || \
+    apt install -y "$*" > /dev/null 2>&1 || \
     yum install -y "$*" > /dev/null 2>&1 || \
     pacman -S --noconfirm "$*" > /dev/null 2>&1
 }
 
 generic_update() {
-    apt update > /dev/null 2>&1 || \
     brew update -y > /dev/null 2>&1 || \
+    apt update > /dev/null 2>&1 || \
     yum update -y > /dev/null 2>&1 || \
     pacman -Sy --noconfirm > /dev/null 2>&1
 }
