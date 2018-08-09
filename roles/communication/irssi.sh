3982fbfeec86f6628c5de6eadcebae7df03ebd22
Denis Costa <deniscostadsc@gmail.com>
2018-08-09 20:43:14 -0300

    Fix some lint

--- roles/communication/irssi.sh
+++ roles/communication/irssi.sh
@@ -5,11 +5,11 @@ kickstart.context 'Communication/Irssi'
 kickstart.package.install irssi
 
 kickstart.info "Add notification plugin"
-kickstart.file.mkdir_and_chown $MYHOME/.irssi/scripts/autorun/
-cp --preserve=mode,ownership files/communication/irssi/irssi-notifier.sh $MYHOME/.bin/
-cp --preserve=mode,ownership files/communication/irssi/notify-listener.py $MYHOME/.bin/notify-listener
-cp --preserve=mode,ownership files/communication/irssi/notify.pl $MYHOME/.irssi/scripts/
-kickstart.file.link $MYHOME/.irssi/scripts/notify.pl $MYHOME/.irssi/scripts/autorun/notify.pl
+kickstart.file.mkdir_and_chown "$MYHOME/.irssi/scripts/autorun/"
+cp --preserve=mode,ownership files/communication/irssi/irssi-notifier.sh "$MYHOME/.bin/"
+cp --preserve=mode,ownership files/communication/irssi/notify-listener.py "$MYHOME/.bin/notify-listener"
+cp --preserve=mode,ownership files/communication/irssi/notify.pl "$MYHOME/.irssi/scripts/"
+kickstart.file.link "$MYHOME/.irssi/scripts/notify.pl" "$MYHOME/.irssi/scripts/autorun/notify.pl"
 
 kickstart.info "Ask for sensitive data about bouncer"
 echo -n "Type your IRC bouncer adress: "
@@ -28,8 +28,8 @@ echo
 [[ ! -z $BOUCER_IRC_PORT ]] && \
 [[ ! -z $BOUCER_IRC_PASSWORD ]] && {
     kickstart.info "Copy Irssi config files and replace sensitive data"
-    cp --preserve=mode,ownership files/communication/irssi/config $MYHOME/.irssi/config
-    sed -i "s/\$BOUCER_IRC_ADRESS/$BOUCER_IRC_ADRESS/" $MYHOME/.irssi/config
-    sed -i "s/\$BOUCER_IRC_PORT/$BOUCER_IRC_PORT/" $MYHOME/.irssi/config
-    sed -i "s/\$BOUCER_IRC_PASSWORD/$BOUCER_IRC_PASSWORD/" $MYHOME/.irssi/config
+    cp --preserve=mode,ownership files/communication/irssi/config "$MYHOME/.irssi/config"
+    sed -i "s/\$BOUCER_IRC_ADRESS/$BOUCER_IRC_ADRESS/" "$MYHOME/.irssi/config"
+    sed -i "s/\$BOUCER_IRC_PORT/$BOUCER_IRC_PORT/" "$MYHOME/.irssi/config"
+    sed -i "s/\$BOUCER_IRC_PASSWORD/$BOUCER_IRC_PASSWORD/" "$MYHOME/.irssi/config"
 } || true
