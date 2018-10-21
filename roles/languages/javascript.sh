d8ee2b70b07a01078d8ce15dad4f8ea961c1a1ca
Denis Costa <deniscostadsc@gmail.com>
2018-10-21 13:20:39 -0300

    Remove git hooks from nodenv repository
    problem on build: https://api.travis-ci.org/v3/job/444396781/log.txt

--- roles/languages/javascript.sh
+++ roles/languages/javascript.sh
@@ -12,6 +12,11 @@ kickstart.command_exists pyenv || {
     kickstart.git.cloneandpull \
         https://github.com/nodenv/node-build.git \
         "$MYHOME/.nodenv/plugins/node-build"
+    # the hooks on this repo are not that important and
+    # the manupulation on background by ctags was break the build because
+    # when chown was running, some files were already deleted.
+    rm -rf "$MYHOME"/.nodenv/.git/hooks/*
+    rm -rf "$MYHOME"/.nodenv/plugins/node-build/.git/hooks/*
 }
 
 kickstart.file.append_file_once files/languages/javascript/bashrc_javascript.sh "$MYHOME/.bashrc"
