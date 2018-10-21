d5551b486ea28f0613f6d73c326ef22c6416f951
Denis Costa <deniscostadsc@gmail.com>
2018-10-20 21:16:04 -0300

    Add docker-compose completion

--- roles/docker.sh
+++ roles/docker.sh
@@ -17,9 +17,13 @@ kickstart.os.is "Ubuntu" && {
     kickstart.package.install docker-ce
 
     [ -f /usr/local/bin/docker-compose ] || {
+        # TODO: get a way to get the newest version
         sudo curl -L \
             "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" \
             -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
+        sudo curl -L \
+            https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/bash/docker-compose \
+            -o /etc/bash_completion.d/docker-compose
     }
 }
