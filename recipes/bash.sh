02ec82e020647fa6bda7f2f63d2d944c5bbd94c6
Denis Costa <deniscostadsc@gmail.com>
2018-10-21 12:08:12 -0300

    Remove manual bashrc append

--- recipes/bash.sh
+++ recipes/bash.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-kickstart.file.append_once() {
-    if ! kickstart.file.contains "$1" "$2"; then
-        echo "$2" >> "$1"
-    fi
-}
-
 kickstart.file.append_file_once() {
     file_to_append=$1
     destiny_file=$2
--- recipes/python.sh
+++ recipes/python.sh
@@ -13,12 +13,5 @@ kickstart.package.install_pyenv() {
         kickstart.git.cloneandpull \
             https://github.com/pyenv/pyenv-virtualenv.git \
             "$MYHOME/.pyenv/plugins/pyenv-virtualenv"
-
-        # shellcheck disable=SC2016
-        kickstart.file.append_once "$MYHOME/.bashrc" 'export PYENV_ROOT="$HOME/.pyenv"'
-        # shellcheck disable=SC2016
-        kickstart.file.append_once "$MYHOME/.bashrc" 'export PATH="$PYENV_ROOT/bin:$PATH"'
-        # shellcheck disable=SC2016
-        kickstart.file.append_once "$MYHOME/.bashrc" 'eval "$(pyenv init -)"'
     }
 }
