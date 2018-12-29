9ce5fea7e7810d4ec264a346c5bbffe4326bd86e
Denis Costa <deniscostadsc@gmail.com>
2018-12-29 15:25:29 -0200

    Update flatpak packages with the whole system

--- roles/os.sh
+++ roles/os.sh
@@ -2,9 +2,16 @@
 
 kickstart.context 'OS'
 
+# shellcheck source=recipes/flatpak.sh
+source recipes/flatpak.sh
+
+kickstart.info "Install flatpak"
+kickstart.package.install_flatpak
+
 kickstart.info "Update OS"
 kickstart.package.update
 kickstart.package.upgrade
+flatpak update
 
 #kickstart.info "Install find and which for Fedora"
 #kickstart.os.is "RHEL" && kickstart.package.install which
