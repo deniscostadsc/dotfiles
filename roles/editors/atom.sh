df292b3685cf0aa40a5bc6f0f31ffdb0786665b3
Denis Costa <deniscostadsc@gmail.com>
2019-02-22 16:14:53 -0300

    Add atom installation with flatpak

--- roles/editors/atom.sh
+++ roles/editors/atom.sh
@@ -2,28 +2,11 @@
 
 kickstart.context 'Editors/Atom'
 
-# shellcheck source=recipes/os.sh
-source recipes/os.sh
+# shellcheck source=recipes/flatpak.sh
+source recipes/flatpak.sh
 
-kickstart.package.install wget
+kickstart.info "Install flatpak"
+kickstart.package.install_flatpak
 
-! kickstart.command_exists atom && kickstart.os.is "Ubuntu" && {
-    kickstart.info "Download Atom deb file"
-    kickstart.mute wget https://atom.io/download/deb
-
-    kickstart.info "Install Aton from deb"
-    kickstart.mute dpkg -i deb || \
-        kickstart.mute apt-get -f install -y && \
-        kickstart.mute dpkg -i deb
-
-    kickstart.info "Remove Atom file"
-    rm deb
-
-    kickstart.info "Install Atom plugins"
-    kickstart.mute apm install --packages-file files/editors/atom/packages.txt
-    kickstart.mute apm upgrade --no-confirm
-}
-
-kickstart.info "Copy Atom config file"
-kickstart.file.mkdir_and_chown "$MYHOME/.atom/"
-kickstart.file.copy files/editors/atom/config.cson "$MYHOME/.atom/"
+kickstart.info "Install atom"
+flatpak install -y flathub io.atom.Atom
