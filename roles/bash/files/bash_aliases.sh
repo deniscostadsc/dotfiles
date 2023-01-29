95dba426f4ba779494fddd898e47788c981ea4da
Denis Costa <deniscostadsc@gmail.com>
2023-01-28 21:02:58 -0300

    Add missing alias file

---
+++ roles/bash/files/bash_aliases.sh
@@ -0,0 +1,7 @@
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias ll='ls -alhF'
+alias ls='ls -G --color=auto'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
