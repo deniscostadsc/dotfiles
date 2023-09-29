e7feedabb33898980b5cca163e272cc73a12de66
Denis Costa <deniscostadsc@gmail.com>
2023-09-29 18:34:10 -0300

    Add test for YAML extension

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 EXIT_STATUS=0
 
 shopt -s globstar nullglob
+
+for file in **/*.yaml; do
+    echo "The file ${file} should have .yml as extension"
+    EXIT_STATUS=1
+done
+
 for file in **/*.yml; do
     if ! grep '^\-\-\-$' "${file}" &> /dev/null; then
         echo "The file ${file} does not has a '---' at the beggining."
