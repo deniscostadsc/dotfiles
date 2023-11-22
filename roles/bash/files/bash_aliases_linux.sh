6db0f30e58f53adb7d311a395c6328311fd20a5a
Denis Costa <deniscostadsc@gmail.com>
2023-11-22 13:54:43 -0300

    Replace copy/paste aliases with clipboard command

--- roles/bash/files/bash_aliases_linux.sh
+++
@@ -1,3 +0,0 @@
-# shellcheck disable=SC2148
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
---
+++ roles/bash/files/bin/clipboard.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# original from: https://gist.github.com/RichardBronosky/56d8f614fab2bacdd8b048fb58d0c0c7
+
+set -euo pipefail
+
+function linux_copy {
+    cat | xclip -selection clipboard
+}
+
+function linux_paste {
+    xclip -selection clipboard -o
+}
+
+function macos_copy {
+    cat | pbcopy
+}
+
+function macos_paste {
+    pbpaste
+}
+
+function stdin_is_pipe_or_redirection {
+    if [[ -p /dev/stdin ]] && [[ ! -t 0 ]]; then
+        return 0
+    fi
+    return 1
+}
+
+function prevent_prompt_from_being_on_the_same_line {
+    # is the stdout a tty
+    if [[ -t 1 ]]; then
+        echo
+    fi
+}
+
+function detect_os {
+    case "$(uname -s)" in
+        Linux*)
+            printf "linux"
+        ;;
+        Darwin*)
+            printf "macos"
+        ;;
+        *)
+            echo "Clipboard only supports Linux and MacOS."
+            exit 1
+        ;;
+    esac
+}
+
+function main {
+    os="$(detect_os)"
+    if stdin_is_pipe_or_redirection; then
+        "${os}_copy"
+    else
+        "${os}_paste"
+        prevent_prompt_from_being_on_the_same_line
+    fi
+}
+
+main
