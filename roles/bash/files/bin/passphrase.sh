3c08ef611f1d274f752468a4cd2c34c86228697e
Denis Costa <deniscostadsc@gmail.com>
2023-11-23 02:40:34 -0300

    Remove old code from passphrase

--- roles/bash/files/bin/passphrase.sh
+++ roles/bash/files/bin/passphrase.sh
@@ -5,28 +5,11 @@ set -euo pipefail
 min_word_length=4
 max_word_length=6
 password_word_count=5
-dice_number=""
-
-function dice2int {
-    dice_base=6
-
-    i=0
-    number=0
-
-    for current_dice_digit in $(fold -w1 <<< "$(echo "$1" | rev)"); do
-        current_dice_digit_index=$((current_dice_digit - 1))
-        number=$(("$number" + "$current_dice_digit_index" * "$dice_base" ** "$i"))
-        i=$(("$i"+1))
-    done
-
-    echo $number
-}
 
 function show_help {
     echo
-    echo "diceware"
+    echo "passphrase"
     echo
-    echo " --dice (-d)"
     echo " --min (-n) sets minimum number of characters in the words"
     echo " --max (-m) sets maximum number of characters in the words"
     echo " --count (-c) sets how many word the password will contain"
@@ -34,24 +17,15 @@ function show_help {
     echo
 }
 
-
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --dice|-d)
-            dice_number=$2
-            if ! [[ $dice_number =~ ^[123456]{1,6}$ ]]; then
-                echo "Invalid dice input!"
-                exit 1
-            fi
-            shift
-        ;;
         --min|-n)
             min_word_length=$2
             shift
         ;;
         --max|-m)
             max_word_length=$2
-            if [[ $max_word_length -lt $min_word_length ]]; then
+            if [[ ${max_word_length} -lt ${min_word_length} ]]; then
                 echo
                 echo "max should be greater than min"
                 exit 1
@@ -75,28 +49,15 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-
 elegible_words=$(
     grep -E "^[A-Za-z]{${min_word_length},${max_word_length}}$" /usr/share/dict/words | tr '\n' ' ')
-word_count=$(wc -w <<< "$elegible_words")
+word_count=$(wc -w <<< "${elegible_words}")
 
+for ((i = 0; i < "${password_word_count}"; i++)); do
+    word_index="$((RANDOM * RANDOM * RANDOM * RANDOM % word_count + 1))"
+    word="$(cut -d ' ' -f "${word_index}" <<< "${elegible_words}" | tr '[:upper:]' '[:lower:]')"
+    echo -n "${word} "
+done
 
-if [[ -z $dice_number ]]; then
-    for ((i = 0; i < "$password_word_count"; i++)); do
-        echo -n "$(cut -d ' ' -f "$((RANDOM % word_count + 1))" <<< "$elegible_words" | tr '[:upper:]' '[:lower:]') "
-    done
-
-    echo
-    exit 0
-fi
-
-if [ "$(dice2int "$dice_number")" -gt "$word_count" ]; then
-    echo
-    echo -n "$dice_number represents the $(dice2int "$dice_number")th word, but"
-    echo " we just have $word_count words."
-    echo
-
-    exit 1
-fi
-
-cut -d ' ' -f "$(dice2int "$dice_number")" <<< "$elegible_words" | tr '[:upper:]' '[:lower:]'
+echo
+exit 0
