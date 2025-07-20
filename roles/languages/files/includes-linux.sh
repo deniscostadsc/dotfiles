03522e79312d2a2aceccd16827cc792e9c7a53b9
Denis Costa <deniscostadsc@gmail.com>
2025-07-20 14:52:25 -0300

    Improve languages task

--- roles/languages/files/includes-linux.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck disable=SC1091,SC2148,SC2154
-source "${HOME}/.asdf/asdf.sh"
-# shellcheck disable=SC1090
-source <(asdf completion bash)
--- roles/languages/files/includes-mac.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck disable=SC1091,SC2148,SC2154
-source "${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh"
-# shellcheck disable=SC1090
-source <(asdf completion bash)
