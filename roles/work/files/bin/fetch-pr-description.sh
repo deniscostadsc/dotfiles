ff62130ede0df7fbae6189d8e96ced48721960c2
Denis Costa <deniscostadsc@gmail.com>
2026-07-04 20:07:34 -0300

    Move script to bin folder

--- roles/work/files/bin/fetch-pr-description.sh
+++ roles/work/files/bin/fetch-pr-description.sh
@@ -7,7 +7,7 @@ description_file=$(current-ticket-pr-decription-file)
 [[ -f "${description_file}" ]] && exit 0
 
 if gh pr view --json number &>/dev/null; then
-    gh pr view --json body --jq '.body' > "${description_file}"
+    gh pr view --json body --jq '.body' >"${description_file}"
 else
     cp "$(git rev-parse --show-toplevel)/.github/PULL_REQUEST_TEMPLATE.md" "${description_file}"
 fi
